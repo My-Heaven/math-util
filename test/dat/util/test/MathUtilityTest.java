783caa0aff929d7131b515f33be7e1fa398a00f2
My-Heaven <tdse2k@gmail.com>
2020-11-17 15:50:32 +0700

    add JUnil library

---
+++ test/dat/util/test/MathUtilityTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dat.util.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tan DaT
+ */
+public class MathUtilityTest {
+    
+    public MathUtilityTest() {
+    }
+    
+}
